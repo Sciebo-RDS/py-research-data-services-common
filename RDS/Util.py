2cf2bc52948f622012f8c86f4179dc446464487f
Peter Heiss <peter.heiss@uni-muenster.de>
2020-08-13 11:27:04 +0200

    bump version, try to fix issue with to_default

--- RDS/Util.py
+++ RDS/Util.py
@@ -139,7 +139,10 @@ def monkeypatch(func_name: str = "to_json"):
     from json import JSONEncoder, JSONDecoder
 
     def to_default(self, obj):
-        return getattr(obj.__class__, func_name, to_default.default)(obj)
+        try:
+            return getattr(obj.__class__, func_name, to_default.default)(obj)
+        except:
+            return getattr(obj.__class__, func_name, to_default.default)()
 
     to_default.default = JSONEncoder.default  # Save unmodified default.
     JSONEncoder.default = to_default  # Replace it.
