0a0e8bda0eaec1b539a5d5884feba4812b2d37db
Peter Heiss <peter.heiss@uni-muenster.de>
2021-08-19 11:18:52 +0200

    add roparser

--- RDS/__init__.py
+++ RDS/__init__.py
@@ -3,6 +3,7 @@ from .Service import *
 from . import ServiceException
 from .Token import *
 from .Informations import *
+from .ROParser import *
 import RDS.Util as Util
 
 
@@ -13,7 +14,7 @@ def int_or_str(value):
         return value
 
 
-__version__ = "0.45"
+__version__ = "0.46"
 VERSION = tuple(map(int_or_str, __version__.split(".")))
 
 __all__ = [
